737c3ed873113016856ec8a1699ee1da53031cd8
jsaouma <jsaouma@css.edu>
2023-10-29 18:39:28 -0500

    Initial commit

---
+++ app/src/main/java/css/cis3334/API.java
@@ -0,0 +1,13 @@
+package css.cis3334;
+
+public class API {
+    private String name;
+    private String id;
+
+    // Getters and setters for 'name' and 'id'
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+}
